f0cb21deca98c2a6b81f87ed3ae5766e862a2473
Lê Đăng Ngôn <_nqonz.deung_@MacBookAir.lan>
2026-08-25 22:58:34 +0700

    Initial commit: Hello World Spring Boot

---
+++ src/main/java/com/example/hello_springboot/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.hello_springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello Spring Boot from GitHub!";
+    }
+}
